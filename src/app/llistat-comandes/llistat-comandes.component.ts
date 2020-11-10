ddd77b6bd46f33d37b13702c13aa3c60f3838e8b
SusannaMontero <49920436+SusannaMontero@users.noreply.github.com>
2020-11-10 10:04:44 +0100

    10/11/20 10:04h
    He comentat tot el codi que dona error, l'error ve de que falten coses per declarar

--- src/app/llistat-comandes/llistat-comandes.component.ts
+++ src/app/llistat-comandes/llistat-comandes.component.ts
@@ -1,26 +1,26 @@
 /*
-AQUI HA DE VENIR LES DADES DEL FORMULARI COM EL DE NELSON QUE HI HA A ALTA EVENT///////////////////
+AQUI HA DE VENIR LES DADES DEL FORMULARI COM EL DE NELSON QUE HI HA A ALTA EVENT///////////////////*/
 
 import { Component, OnInit } from '@angular/core';
-/*import { NomComanda}  from 'src/app/models/comanda.model';
+/*import { NomComanda}  from 'src/app/models/comanda.model';*/
 
 @Component({
   selector: 'app-llistat-comandes',
   templateUrl: './llistat-comandes.component.html',
   styleUrls: ['./llistat-comandes.component.css']
 })
-/*export class LlistatComandesComponent implements OnInit {
+export class LlistatComandesComponent implements OnInit {
 
  /* comandes: NomComanda [] = [];
-  comandaSelected: NomComanda = null;
+  comandaSelected: NomComanda = null;*/
 
   constructor() { }
 
   ngOnInit(): void {
-    this.comandes.push(new NomComanda('Pedro'));
+   /* this.comandes.push(new NomComanda('Pedro'));
     this.coamndes.push(new NomComanda('Juan'));
     this.coamndes.push(new NomComanda('Jorge'));
-    this.coamndes.push(new NomComanda('Borja'));
+    this.coamndes.push(new NomComanda('Borja'));*/
   }
 
-}*/
+}
