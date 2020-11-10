78888d315fccb77d3635cf5f70920b09a9bc7159
SusannaMontero <49920436+SusannaMontero@users.noreply.github.com>
2020-11-10 12:39:16 +0100

    10/11/20 12:38
    afegit formulari alta events

--- src/app/alta-event/alta-event.component.ts
+++ src/app/alta-event/alta-event.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LlistatComandesComponent } from '../llistat-comandes/llistat-comandes.component';
 
 @Component({
   selector: 'app-alta-event',
@@ -12,4 +13,30 @@ export class AltaEventComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  mostrarInfo = "";
+  mostrarMensaje = "";
+
+  EnviarDatos(nombre, lloc, email, mensaje){
+
+    if(nombre.value =="") {
+      alert("El nombre esta vacío");
+    }
+
+    if(lloc.value =="") {
+      this.mostrarInfo="Error!, escriu una població";
+
+    if(email.value == "") {
+      this.mostrarInfo="Error!, escriu el teu email";
+    }
+
+
+    }else {
+      this.mostrarInfo="Incorrecto";
+    }
+
+    if(mensaje.value.length <= 2) {
+      this.mostrarMensaje = "El texto introducido es demasiado corto";
+    }
+  }
 }
